1be150bc8d2228894bf6a594ffbc90cb051b7345
MarianoANDR <mariano.uk2022@gmail.com>
2022-11-21 11:07:51 +0000

    commits

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,47 @@
+import './footer.css';
+
+const Footer = () => {
+   
+    return(
+        
+         <footer className='footer'>
+        <div className = 'footDiv1'>
+            <span>Name of company</span>
+            <p>Name of company is a 501c3 nonprofit</p>
+        </div>   
+        <div className='footDiv2'>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+        </div>
+        <div className='footDiv3'>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+        </div>
+        <div className='footDiv4'>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+        </div>
+        <div className='footDiv5'>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+        </div>
+        </footer>
+        
+       
+    ) 
+    
+}
+
+export default Footer;
